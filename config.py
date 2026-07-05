0ac233ad5c864e832577d2de0e097a90c22eedef
24f2005879-beep <24f2005879@ds.study.iitm.ac.in>
2026-07-05 22:49:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,8 +24,8 @@ dQIDAQAB ...
 
 # 4. Q3: 12-Factor Config (Manually merge the variables)
 Q3_PORT = 8979
-Q3_WORKERS = 1
-Q3_DEBUG = True
+Q3_WORKERS = 15
+Q3_DEBUG = False
 Q3_LOG_LEVEL = "info"
 
 # 5. Q5: Analytics (Find the API key in the Q5 instruction tab)
